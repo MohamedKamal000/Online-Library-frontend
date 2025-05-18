cf78ccb5ee1cbd972efe1a675c82086fd33cb14d
Mohamed Adel Kamal <raynowing007@gmail.com>
2025-05-19 02:50:11 +0300

    tiny change to build vercel deployment

--- backend/settings.py
+++ backend/settings.py
@@ -15,6 +15,8 @@ import os
 import cloudinary
 from cloudinary.uploader import upload
 import cloudinary.api
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
