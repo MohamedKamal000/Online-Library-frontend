3f4383a39aed7c2dddaa69b61f8990fb0221752e
Mohamed Adel Kamal <raynowing007@gmail.com>
2025-05-19 03:11:58 +0300

    trying to fix this error on production

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -16,3 +16,4 @@ settings_module = 'backend.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
+app = application
